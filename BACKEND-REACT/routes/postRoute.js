e83d9998d7c0dd9a63bc780e242603cab1026653
shekharkourav <shekharkourav5424@gmail.com>
2024-04-09 15:39:14 +0530

    add multer

--- BACKEND-REACT/routes/postRoute.js
+++ BACKEND-REACT/routes/postRoute.js
@@ -4,3 +4,8 @@ const app=express();
 const bodyParser=require('body-parser');
 post_route.use(bodyParser.json());
 post_route.use(bodyParser.urlencoded({extended:true}));
+
+const multer=require('multer');
+const path=require('path');
+
+post_route.use(express.static('public'));
