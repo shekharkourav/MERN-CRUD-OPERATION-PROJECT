a9fd70dec467fee0fb1c79c1261749183dc4ed1c
shekharkourav <shekharkourav5424@gmail.com>
2024-04-10 15:06:30 +0530

    create and connect to database

--- BACKEND-REACT/index.js
+++ BACKEND-REACT/index.js
@@ -9,6 +9,9 @@ app.use(cors({
 const mongoose=require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/BACKEND_REACT");
 
+const post_route=require('./routes/postRoute');
+app.use('/api',post_route);
+
 app.listen(8080,function(){
     console.log("server is running");
 });
